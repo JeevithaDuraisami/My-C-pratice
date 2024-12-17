ce4fd9355453ec4a153ef78e19907224f2978063
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:29:08 +0530

    Create greatest of 2 num.c

---
+++ greatest of 2 num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the first number:");
+    scanf("%d\n",&a);
+    printf("enter the second number:");
+    scanf("%d\n",&b);
+    if(a>b)
+     
+      printf("%d\n is greater",a);
+     else
+      printf("%d\n is greater",b);
+    //Fill the code
+    return 0;
+}
