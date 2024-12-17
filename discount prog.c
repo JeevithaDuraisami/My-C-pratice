32234b9b3317459ef9dea0ffd91f437103434474
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:25:42 +0530

    Create discount prog.c

---
+++ discount prog.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    float d,e,f,g;
+    d=b*c;
+    e=a+c;
+    f=(d*2)/100;
+    g=e-f;
+    printf("%.2f\n",d);
+    printf("%.2f\n",e);
+    printf("%.2f\n",f);
+    printf("%.2f\n",g);
+    
+    
+    
+    return 0;
+}
