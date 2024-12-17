d1b9c2eaa286ab138f82e19aa557e68ea771cb73
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:19:04 +0530

    Create decimal values.c

---
+++ decimal values.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   float n;
+   scanf("%f",&n);
+   printf("%f\n",n);
+   printf("%.3f\n",n);
+   printf("%.2f\n",n);
+   printf("%.1f\n",n);
+   
+    return 0;
+}
