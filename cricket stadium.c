4c3e8f1e5c0ba157d50ebc1e3df6239adb38fe71
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:11:22 +0530

    Create cricket stadium.c

---
+++ cricket stadium.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c=2*(a+b);
+    d=a*b;
+    printf("%d\n",c);
+    printf("%d\n",d);
+    
+    
+    return 0;
+}
