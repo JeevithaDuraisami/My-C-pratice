c51d4d177994b0e0170a403438ad5fe7e2f6305e
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:30:01 +0530

    Create positive or negative .c

---
+++ positive or negative .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=0)
+      printf("%d positive",a);
+    else
+      printf("%d negative",a);
+    //Fill the code
+    return 0;
+}
