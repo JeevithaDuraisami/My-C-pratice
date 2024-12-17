d5cf6f8a80fdd77692ae556a336c88c591179ee3
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:28:10 +0530

    Create win the war prog.c

---
+++ win the war prog.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    int n=;
+    int m=a+b;
+    printf("%d %d",n,m);
+    return 0;
+}
