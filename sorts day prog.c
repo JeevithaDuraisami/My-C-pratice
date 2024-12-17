8b253c2f143d18aeac47ca25cfaab1d86eefcbc8
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:26:29 +0530

    Create sorts day prog.c

---
+++ sorts day prog.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int c,d;
+    c=a/b;
+    d=a%b;
+    printf("%d\n",c);
+    printf("%d\n",d);
+    
+    return 0;
+}
