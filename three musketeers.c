43ba7f523e4a36d9bf96e7a13131d1a669aca49c
JeevithaDuraisami <jeevithajeevi301@gmail.com>
2024-12-17 16:27:27 +0530

    Create three musketeers.c

---
+++ three musketeers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   float a,b,c,d;
+   scanf("%f",&a);
+   scanf("%f",&b);
+   scanf("%f",&c);
+   scanf("%f",&d);
+   float e,f;
+   e=((a+c)/2);
+   f=((b+d)/2);
+   printf("%.1f\n",e);
+   printf("%.1f\n",f);
+    return 0;
+}
